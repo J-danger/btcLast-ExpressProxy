1e88961069bebe7b0b5202de5b197b990df7eb25
Stephen Howe <Stevehowe10@gmail.com>
2020-03-08 17:17:06 -0500

    test

--- server.js
+++ server.js
@@ -6,17 +6,10 @@ const axios = require('axios').default;
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-if (process.env.NODE_ENV === 'production') {
-  // Exprees will serve up production assets
-  app.use(express.static('client/build'));
-
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+app.use(express.static(path.join(__dirname, 'client')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'index.html'));
   });
-  app.disable('etag');
-}
 
 
 
